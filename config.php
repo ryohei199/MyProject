60ce9af8b2d18a46ea1ee9c0289d4a3eede4e45c
Ryohei Kawasaki <st022387@m02.kyoto-kcg.ac.jp>
2025-01-08 19:47:56 +0900

    First commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'bulletin_board',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+    'DB_PORT' => '3306',
+    'SITE_NAME' => '2ちゃんねる風掲示板',
+    'MAX_THREAD_RESPONSES' => 1000,
+    'RESPONSES_PER_PAGE' => 50,
+];
